d44f7309b33066c03bc66a2b9534a0db42ea3e9f
jeffreybiles <bilesjeffrey@gmail.com>
2019-04-28 08:40:11 -0500

    isEmpty not needed

--- app/components/list-gist.js
+++ app/components/list-gist.js
@@ -1,12 +1,6 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { computed } from "@ember/object";
 
 export default class ListGistComponent extends Component {
   @service gistsInfo;
-
-  @computed("gistsInfo")
-  get isEmpty() {
-    return this.gistsInfo.gists.length > 0;
-  }
 }
