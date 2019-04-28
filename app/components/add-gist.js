095cf2ba26278b7d323660bf9ea976c213f736dc
jeffreybiles <bilesjeffrey@gmail.com>
2019-04-28 08:39:31 -0500

    remove some unused stuff

--- app/components/add-gist.js
+++ app/components/add-gist.js
@@ -4,7 +4,6 @@ import Component from "@glimmer/component";
 export default class AddGistComponent extends Component {
   @action
   addFile(body, description, fileName) {
-    const blah = 5;
     this.args.addGistInfo(body, description, fileName);
   }
 }
